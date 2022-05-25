012a29172b750909894ea89fa58d2c5c773a8cc9
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-05-25 16:11:08 +0200

    working on home

---
+++ resources/views/partials/home.blade.php
@@ -0,0 +1,27 @@
+@extends('../index')
+
+@section('main')
+<main>
+    ciaooooooooooooooooo
+    {{-- <div class="container_cards py-4">
+        <div class="container-sm">
+            <div class="tag_series">CURRENT SERIES</div>
+            <div class="row">
+                @foreach ($arrayComics as $comic)
+                <div class="col-sm-2">
+                    <div class="wrapper_img">
+                        <a href="#">
+                            <img class="pt-5 pb-3" src="{{$comic['thumb']}}" alt="{{$comic['title']}}" />
+                        </a>
+                    </div>
+                    <p>{{ $comic["series"] }}</p>
+                </div>
+                @endforeach
+            </div>
+            <div class="row mt-5 justify-content-center">
+                <div class="col-auto tag_load">LOAD MORE</div>
+            </div>
+        </div>
+    </div> --}}
+</main>
+@endsection
