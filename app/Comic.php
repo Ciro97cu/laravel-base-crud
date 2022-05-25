e64e62752e473dfb3c3f9340444a4b06ea93d2c7
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-05-25 14:51:12 +0200

    defined table name in model file

--- app/Comic.php
+++ app/Comic.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comic extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = "comics";
 }
