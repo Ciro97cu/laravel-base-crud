d5f62a7f635d0687fe757c23fbd7b316d2868c94
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-05-25 15:01:51 +0200

    populated table with seeder file

--- database/seeds/ComicTableSeeder.php
+++ database/seeds/ComicTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Comic;
 
 class ComicTableSeeder extends Seeder
 {
@@ -11,6 +12,17 @@ class ComicTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $arrayComics = config("comics");
+        foreach ($arrayComics as $comic) {
+            $newComic = new Comic();
+            $newComic->title = $comic["title"];
+            $newComic->description = $comic["description"];
+            $newComic->thumb = $comic["thumb"];
+            $newComic->price = $comic["price"];
+            $newComic->series = $comic["series"];
+            $newComic->sale_date = $comic["sale_date"];
+            $newComic->type = $comic["type"];
+            $newComic->save();
+        }
     }
 }
