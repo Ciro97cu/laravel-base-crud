140af52d647f25d5fd32b8fa9a0eec3fa34e7011
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-05-26 20:55:28 +0200

    added form for destroy

--- resources/views/partials/index.blade.php
+++ resources/views/partials/index.blade.php
@@ -8,7 +8,15 @@
             <div class="row">
                 @foreach ($arrayComics as $comic)
                 <div class="col-sm-2">
-                    <a href="{{ route('partials.edit', $comic->id) }}" class="edit_card mt-5 mb-2 p-1 text-white text-center d-block">EDIT</a>
+                    <div class="d-flex gap-1">
+                        <a href="{{ route('partials.edit', $comic->id) }}" class="edit_card mt-5 mb-2 p-1 text-white text-center w-50">EDIT</a>
+                        <form action="{{ route("partials.destroy", $comic->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete the comic?');">
+                            @csrf
+                            @method('DELETE')
+                            <input type="submit" value="DELETE" class="edit_card mt-5 mb-2 p-1 text-white text-center border-0">
+                        </form>
+                        {{-- <a href="{{ route('partials.destroy', $comic->id) }}" class="edit_card mt-5 mb-2 p-1 text-white text-center w-50">DELETE</a> --}}
+                    </div>
                     <div class="wrapper_img">
                         <a href="{{ route('partials.show', $comic->id) }}">
                             <img class="pb-3" src="{{$comic['thumb']}}" alt="{{$comic['title']}}" />
