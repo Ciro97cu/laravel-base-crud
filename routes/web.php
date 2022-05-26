98b6b4bf70175c117df3f4c442d470a6e972a07d
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-05-26 14:29:06 +0200

    added redirect

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get("/", function () {
+    return redirect()->route("partials.index");
+});
+
 Route::resource('partials', "ComicController");
