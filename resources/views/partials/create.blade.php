8eb1ad6cd4ece6c51ca6be79e049261344b93a22
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-05-27 19:00:56 +0200

    changed input text with date

--- resources/views/partials/create.blade.php
+++ resources/views/partials/create.blade.php
@@ -8,28 +8,28 @@
                 
                 @csrf
 
-                    <label for="title">Title</label>
-                    <input type="text" name="title" placeholder="Add Title">
+                <label for="title">Title</label>
+                <input type="text" name="title" placeholder="Add Title">
+        
+                <label for="description">Description</label>
+                <textarea name="description" form="comicsform" placeholder="Add Description"></textarea>
+        
+                <label for="thumb">Thumb</label>
+                <input type="text" name="thumb" placeholder="Add Thumb">
+    
+                <label for="price">Price</label>
+                <input type="text" name="price" placeholder="Add Price">       
             
-                    <label for="description">Description</label>
-                    <textarea name="description" form="comicsform" placeholder="Add Description"></textarea>
+                <label for="series">Series</label>
+                <input type="text" name="series" placeholder="Add Series">
+                            
+                <label for="sale_date">Sale date</label>
+                <input type="date" name="sale_date" placeholder="Add Sale date">
             
-                    <label for="thumb">Thumb</label>
-                    <input type="text" name="thumb" placeholder="Add Thumb">
-     
-                    <label for="price">Price</label>
-                    <input type="text" name="price" placeholder="Add Price">       
-                
-                    <label for="series">Series</label>
-                    <input type="text" name="series" placeholder="Add Series">
-                               
-                    <label for="sale_date">Sale date</label>
-                    <input type="text" name="sale_date" placeholder="Add Sale date">
-                
-                    <label for="type">Type</label>
-                    <input type="text" name="type" placeholder="Add Type">
-                
-                    <input type="submit">
+                <label for="type">Type</label>
+                <input type="text" name="type" placeholder="Add Type">
+            
+                <input type="submit">
             
             </form>
         </div>
--- resources/views/partials/edit.blade.php
+++ resources/views/partials/edit.blade.php
@@ -8,30 +8,30 @@
                 
                 @csrf
 
-                    @method("PUT")
+                @method("PUT")
 
-                    <label for="title">Title</label>
-                    <input type="text" name="title" placeholder="Add Title" value="{{ $comic->title }}">
+                <label for="title">Title</label>
+                <input type="text" name="title" placeholder="Add Title" value="{{ $comic->title }}">
+        
+                <label for="description">Description</label>
+                <textarea name="description" form="comicsform" placeholder="Add Description"></textarea>
+        
+                <label for="thumb">Thumb</label>
+                <input type="text" name="thumb" placeholder="Add Thumb" value="{{ $comic->thumb }}">
+    
+                <label for="price">Price</label>
+                <input type="text" name="price" placeholder="Add Price" value="{{ $comic->price }}">       
             
-                    <label for="description">Description</label>
-                    <textarea name="description" form="comicsform" placeholder="Add Description"></textarea>
+                <label for="series">Series</label>
+                <input type="text" name="series" placeholder="Add Series" value="{{ $comic->series }}">
+                            
+                <label for="sale_date">Sale date</label>
+                <input type="date" name="sale_date" placeholder="Add Sale date" value="{{ $comic->sale_date }}">
             
-                    <label for="thumb">Thumb</label>
-                    <input type="text" name="thumb" placeholder="Add Thumb" value="{{ $comic->thumb }}">
-     
-                    <label for="price">Price</label>
-                    <input type="text" name="price" placeholder="Add Price" value="{{ $comic->price }}">       
-                
-                    <label for="series">Series</label>
-                    <input type="text" name="series" placeholder="Add Series" value="{{ $comic->series }}">
-                               
-                    <label for="sale_date">Sale date</label>
-                    <input type="text" name="sale_date" placeholder="Add Sale date" value="{{ $comic->sale_date }}">
-                
-                    <label for="type">Type</label>
-                    <input type="text" name="type" placeholder="Add Type" value="{{ $comic->type }}">
-                
-                    <input type="submit">
+                <label for="type">Type</label>
+                <input type="text" name="type" placeholder="Add Type" value="{{ $comic->type }}">
+            
+                <input type="submit">
             
             </form>
         </div>
